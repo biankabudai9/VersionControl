9c2fe39cbbb159b79d9c0c2f4ea65f2e004666d0
Bianka Budai <bianka.budai@stud.uni-corvinus.hu>
2020-09-27 23:36:07 +0200

    Commit changes

--- usermaintenance/usermaintenance/Form1.cs
+++ usermaintenance/usermaintenance/Form1.cs
@@ -14,7 +14,13 @@ namespace usermaintenance
     {
         public Form1()
         {
+
             InitializeComponent();
+
+            label1.Text = Resource1.LastName;
+            label2.Text = Resource1.FirstName;
+            button1.Text = Resource1.Add;
+
         }
     }
 }
