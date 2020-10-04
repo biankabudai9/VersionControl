56b6a23ae6fea3d098487c12764240131b1a1e94
Bianka Budai <bianka.budai@stud.uni-corvinus.hu>
2020-10-04 21:27:24 +0200

    Commit

--- usermaintenance/usermaintenance/Form1.Designer.cs
+++ usermaintenance/usermaintenance/Form1.Designer.cs
@@ -41,18 +41,18 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(407, 62);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(66, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "firstname";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(407, 123);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.Size = new System.Drawing.Size(65, 17);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "lastname";
             // 
             // textBox1
             // 
@@ -83,7 +83,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(256, 23);
             this.button1.TabIndex = 5;
-            this.button1.Text = "Hozzáadás";
+            this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
