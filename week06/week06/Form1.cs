bb0ac687bfcc6d70b4a70e8b0274f616480fdbef
Bianka Budai <bianka.budai@stud.uni-corvinus.hu>
2020-10-18 23:18:43 +0200

    Commit_6

--- week06/week06/Form1.cs
+++ week06/week06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using week06.Entities;
 using week06.MnbServiceReference;
@@ -68,7 +69,29 @@ namespace week06
                     rate.Value = value / unit;
 
 
+                //Chart
 
+                chartRateData.DataSource = Rates;
+
+
+                var series = chartRateData.Series[0];
+                series.ChartType = SeriesChartType.Line;
+
+                series.XValueMember = "Date";
+                series.YValueMembers = "Value";
+                series.BorderWidth = 2;
+
+
+                var legend = chartRateData.Legends[0];
+
+                legend.Enabled = false;
+
+                var chartArea = chartRateData.ChartAreas[0];
+                
+
+                chartArea.AxisX.MajorGrid.Enabled = false;
+                chartArea.AxisY.MajorGrid.Enabled = false;
+                chartArea.AxisY.IsStartedFromZero = false;
             }
         }
     }
