6aefc180f16912a403f41cb77f294266e3022b80
Bianka Budai <bianka.budai@stud.uni-corvinus.hu>
2020-10-25 22:45:18 +0100

    Commit_3

---
+++ week07/week07/Entities/BirthProbability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07.Entities
+{
+    class BirthProbability
+    {
+        public int age { get; set; }
+        public int kids { get; set; }
+        public double P { get; set; }
+
+
+    }
+}
---
+++ week07/week07/Entities/DeathProbability.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07.Entities
+{
+    class DeathProbability
+    {
+
+
+        public int gender { get; set; }
+        public int age { get; set; }
+        public double P { get; set; }
+
+    }
+}
