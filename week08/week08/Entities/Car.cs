5c2810ed9dcaa293789a1e45449023148f1c1889
Bianka Budai <bianka.budai@stud.uni-corvinus.hu>
2020-11-15 21:26:00 +0100

    Commit_8

---
+++ week08/week08/Entities/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using week08.Abstractions;
+
+namespace week08.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics graphics)
+        {
+            Image imageFile = Image.FromFile("irf/car.png");
+            graphics.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+
+        }
+    }
+}
