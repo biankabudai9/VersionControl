5be46f13d499f331561823b02b7b106fa0e414b1
Bianka Budai <bianka.budai@stud.uni-corvinus.hu>
2020-11-22 22:45:54 +0100

    Commit_6

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -21,13 +21,37 @@ namespace UnitTestExample.Test
         ]
         public void TestValidateEmail (string email, bool expectedresult)
         {
-
+           //Arrange
             var accountController = new AccountController();
-          
+          //Act
             var actualResult = accountController.ValidateEmail(email);
-          
+          //Assert
             Assert.AreEqual(expectedresult, actualResult);
 
+        }
+
+
+
+        [
+          Test,
+          TestCase("Aaaaaaaaaa", false),
+          TestCase("AAAAAAAAA", false),
+          TestCase("aaaaaaaaa", false),
+          TestCase("aAa3", false),
+          TestCase("AaAaAaAaA9", true),
+      ]
+
+        public void TestPassword(string password, bool expectedResult)
+        {
+            
+            //Arrange
+            AccountController accountController = new AccountController();
+
+            //Act
+            var actualResult = accountController.ValidatePassword(password);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
